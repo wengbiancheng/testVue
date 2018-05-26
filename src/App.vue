cc941c18275d327da6b16bb1e1c8a64b0edcf5a4
wengbiancheng <1569702426@qq.com>
2018-05-26 21:34:52 +0800

    引入border=1px，即最小的物理像素1px显示分割线的功能

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <v-header :seller="seller"></v-header>
-    <div class="tab">
+    <div class="tab border-1px">
       <div class="tab-item">
         <router-link to="/goods">商品</router-link>
       </div>
@@ -46,13 +46,22 @@
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  @import "./common/stylus/mixin.styl"
+
   #app
     .tab
       display: flex
       width: 100%
       height: 40px
       line-height: 40px
+      border-1px(rgba(7, 17, 27, 0.1))
       .tab-item
         flex: 1
         text-align: center
+        & > a
+          display: block
+          font-size: 14px
+          color: rgb(77, 85, 93)
+          &.active
+            color: rgb(240, 20, 20)
 </style>
--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import VueRouter from 'vue-router';
 import goods from 'components/goods/goods';
 import ratings from 'components/ratings/ratings';
 import seller from 'components/seller/seller';
+import 'common/stylus/index.styl';
 
 Vue.config.productionTip = false;
 
