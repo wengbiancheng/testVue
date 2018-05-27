39e60b170bda0f229661e32c1b9d540fb8baf1b4
wengbiancheng <1569702426@qq.com>
2018-05-27 18:25:29 +0800

    多行图片文字的v-for优惠信息显示

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -17,7 +17,6 @@
           <span class="text">{{seller.supports[0].description}}</span>
         </div>
       </div>
-      d
       <div v-if="seller.supports" class="support-count" @click="showDetail">
         <span class="count">{{seller.supports.length}}个</span>
         <i class="icon-keyboard_arrow_right"></i>
@@ -40,6 +39,17 @@
           <div class="title">
             <div class="line"></div>
             <div class="text">优惠信息</div>
+            <div class="line"></div>
+          </div>
+          <ul v-if="seller.supports" class="supports">
+            <li class="support-item" v-for="(item,index) in seller.supports" v-bind:key="index">
+              <span class="icon" :class="classMap[seller.supports[index].type]"></span>
+              <span class="text">{{seller.supports[index].description}}</span>
+            </li>
+          </ul>
+          <div class="title">
+            <div class="line"></div>
+            <div class="text">商家公告</div>
             <div class="line"></div>
           </div>
         </div>
@@ -217,7 +227,7 @@
           .title
             display: flex
             width: 80%
-            margin: 30px auto 24px auto
+            margin: 28px auto 24px auto
             .line
               flex: 1
               position: relative
@@ -225,12 +235,45 @@
               border-bottom: 1px solid rgba(255, 255, 255, 0.2)
             .text
               padding: 0px 12px
+              font-weight: 700
               font-size 14px
-      .detail-close
-        position: relative
-        width: 32px
-        height: 32px
-        font-size: 32px
-        margin: -64px auto 0px auto
-        clear: both
+          .supports
+            width: 80%
+            margin: 0px auto
+            .support-item
+              padding: 0px 12px
+              margin-bottom: 12px
+              font-size: 0
+              &:last-child
+                margin-bottom: 0px
+              .icon
+                display: inline-block
+                width: 16px
+                height: 16px
+                vertical-align: top
+                margin-right: 6px
+                background-size: 16px 16px
+                background-repeat: no-repeat
+                &.decrease
+                  bg-image('decrease_2')
+                &.discount
+                  bg-image('discount_2')
+                &.guarantee
+                  bg-image('guarantee_2')
+                &.invoice
+                  bg-image('invoice_2')
+                &.special
+                  bg-image('special_2')
+
+              .text
+                line-height: 16px
+                font-size: 12px
+
+  .detail-close
+    position: relative
+    width: 32px
+    height: 32px
+    font-size: 32px
+    margin: -64px auto 0px auto
+    clear: both
 </style>
