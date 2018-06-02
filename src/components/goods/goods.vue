94f35e97d35ca47984d28fe2d04c2e31466ec8d2
wengbiancheng <1569702426@qq.com>
2018-06-03 00:19:49 +0800

    完成goods模块的右侧商品列表的基本布局和样式(按钮还没有加上去)

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -9,7 +9,32 @@
         </li>
       </ul>
     </div>
-    <div class="foods-wrapper"></div>
+    <div class="foods-wrapper">
+      <ul>
+        <li v-for="(item,index) in goods" class="food-list" v-bind:key="index">
+          <h1 class="title">{{item.name}}</h1>
+          <ul>
+            <li v-for="(food,index1) in item.foods" class="food-item" v-bind:key="index1">
+              <div class="icon">
+                <img :src="food.icon" alt="商品图片" width="57px" height="57px">
+              </div>
+              <div class="content">
+                <h2 class="name">{{food.name}}</h2>
+                <p class="desc">{{food.description}}</p>
+                <div class="extra">
+                  <span class="count">月售{{food.sellCount}}份</span>
+                  <span>好评率{{food.rating}}%</span>
+                </div>
+                <div class="price">
+                  <span class="now"><span class="now-flag">￥</span>{{food.price}}</span>
+                  <span v-show="food.oldPrice" class="old">￥{{food.oldPrice}}</span>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -84,6 +109,56 @@
           width: 56px
           border-1px(rgba(7, 17, 27, 0.1))
           font-size: 12px
-    .food-wrapper
+    .foods-wrapper
       flex: 1
+      .title
+        padding-left: 14px
+        height: 26px
+        line-height: 26px
+        font-size: 12px
+        border-left: 2px solid #d9dde1
+        color: rgb(147, 153, 159)
+        background: #f3f5f7
+      .food-item
+        display: flex
+        margin: 18px
+        padding-bottom: 18px
+        border-1px(rgba(7, 17, 27, 0.1))
+        &:last-child
+          border-none()
+          margin-bottom: 0
+        .icon
+          flex: 0 0 57px
+          margin-right: 10px
+        .content
+          flex: 1
+          .name
+            margin: 2px 0px 8px 0px
+            height: 14px
+            line-height: 14px
+            font-size: 14px
+            color: rgb(7, 17, 27)
+          .desc, .extra
+            line-height: 10px
+            font-size: 10px
+            color: rgb(7, 17, 27)
+          .desc
+            margin-bottom: 8px
+          .extra
+            .count
+              margin-right: 12px
+          .price
+            font-weight: 700
+            line-height: 24px
+            .now
+              margin-right: 8px
+              font-size: 14px
+              color: rgb(147, 153, 159)
+              .now-flag
+                font-size: 10px
+            .old
+              font-size: 10px
+              text-decoration: line-through
+              color: rgb(147, 153, 159)
+
 </style>
