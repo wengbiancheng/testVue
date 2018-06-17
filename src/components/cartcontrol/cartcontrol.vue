362d5e60a9aafe81f168dd8d115c6666d20c15bc
wengbiancheng <1569702426@qq.com>
2018-06-17 14:32:37 +0800

    goods模块完成右边布局item中减号button的显示和隐藏动画

--- src/components/cartcontrol/cartcontrol.vue
+++ src/components/cartcontrol/cartcontrol.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="cartcontrol">
-    <div class="cart-decrease icon-remove_circle_outline" v-show="food.count>0" @click="decreaseCart"></div>
+    <transition name="move">
+      <div class="cart-decrease" v-show="food.count>0" @click="decreaseCart">
+        <span class="inner icon-remove_circle_outline"></span>
+      </div>
+    </transition>
     <div class="cart-count" v-show="food.count>0">{{food.count}}</div>
     <div class="cart-add icon-add_circle" @click.stop.prevent="addCart"></div>
   </div>
@@ -8,6 +12,7 @@
 
 <script type="text/ecmascript-6">
   import Vue from 'vue';
+
   export default {
     props: {
       food: {
@@ -43,20 +48,42 @@
 <style lang="stylus" rel="stylesheet/stylus">
   .cartcontrol
     font-size: 0
-    .cart-decrease, .cart-add
+    .cart-decrease
       display: inline-block
       padding: 6px
-      line-height: 24px
-      height: 24px
-      font-size: 24px
-      color: rgb(0, 160, 220)
-    .cart-count
-      display: inline-block
-      vertical-align :top
-      padding-top :6px
-      text-align:center
-      width:12px
-      line-height:24px
-      font-size:10px
-      color: rgb(147,153,159)
+      opacity: 1
+      transform: translate3d(0, 0, 0)
+      .inner
+        display: inline-block
+        line-height: 24px
+        height: 24px
+        font-size: 24px
+        color: rgb(0, 160, 220)
+        transform: rotate(0)
+      &.move-enter-active, &.move-leave-active
+        transition: all 0.4s linear
+        .inner
+          transition: all 0.4s linear
+      &.move-enter, &.move-leave-active
+        opacity: 0
+        transform: translate3d(24px, 0)
+        .inner
+          transform: rotate(180deg)
+  .cart-count
+    display: inline-block
+    vertical-align: top
+    padding-top: 6px
+    text-align: center
+    width: 12px
+    line-height: 24px
+    font-size: 10px
+    color: rgb(147, 153, 159)
+
+  .cart-add
+    display: inline-block
+    padding: 6px
+    line-height: 24px
+    height: 24px
+    font-size: 24px
+    color: rgb(0, 160, 220)
 </style>
