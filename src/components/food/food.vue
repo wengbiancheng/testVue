7f616f3b6e431b9e863288de3e0e9835ca2cf566
wengbiancheng <1569702426@qq.com>
2018-06-19 23:23:11 +0800

    goods模块完成商品详情页的头部image布局和样式

---
+++ src/components/food/food.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <transition name="move">
+      <div class="food" v-show="showFlag">
+        <div class="food-content">
+          <div class="image-header">
+            <img :src="food.image">
+            <div class="back" @click.stop.prevent="hide">
+              <i class="icon-arrow_lift"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    props: {
+      food: {
+        type: Object
+      },
+      showFlag: {
+        type: Boolean,
+        default: false
+      }
+    },
+    methods: {
+      hide: function () {
+        this.showFlag = false;
+      }
+    }
+  };
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  .food
+    position: fixed
+    left: 0
+    top: 0
+    bottom: 48px
+    z-index: 30
+    width: 100%
+    background: #fff
+    transform: translate3d(0, 0, 0)
+    &.move-enter-active, &.move-leave-active
+      transition: all 0.2s linear
+    &.move-enter, &.move-leave-active
+      transform: translate3d(100%, 0, 0)
+    .image-header
+      position: relative
+      width: 100%
+      height: 0
+      padding-top: 100%
+      img
+        position: absolute
+        top: 0
+        left: 0
+        width: 100%
+        height: 100%
+      .back
+        position: absolute
+        top: 10px
+        left: 0
+        .icon-arrow_lift
+          display: block
+          padding: 10px
+          font-size: 20px
+          color: #fff
+</style>
