ba50a1032d44eef5abd6abe554a5b9697222c8fe
wengbiancheng <1569702426@qq.com>
2018-07-03 23:30:27 +0800

    ratings模块完成底部评论列表的布局和better-scroll的引入

--- src/components/ratings/ratings.vue
+++ src/components/ratings/ratings.vue
@@ -1,42 +1,110 @@
 <template>
-  <div class="ratings">
-    <div class="overview">
-      <div class="overview-left">
-        <h1 class="score">{{seller.score}}</h1>
-        <div class="title">综合评分</div>
-        <div class="rank">高于周边商家{{seller.rankRate}}%</div>
+  <div class="ratings" ref="ratings">
+    <div class="ratings-content">
+      <div class="overview">
+        <div class="overview-left">
+          <h1 class="score">{{seller.score}}</h1>
+          <div class="title">综合评分</div>
+          <div class="rank">高于周边商家{{seller.rankRate}}%</div>
+        </div>
+        <div class="overview-right">
+          <div class="score-wrapper">
+            <span class="title">服务态度</span>
+            <star :size="36" :score="seller.serviceScore"></star>
+            <span class="score">{{seller.serviceScore}}</span>
+          </div>
+          <div class="score-wrapper">
+            <span class="title">商品评分</span>
+            <star :size="36" :score="seller.foodScore"></star>
+            <span class="score">{{seller.foodScore}}</span>
+          </div>
+          <div class="delivery-wrapper">
+            <span class="title">送达时间</span>
+            <span class="delivery">{{seller.deliveryTime}}分钟</span>
+          </div>
+        </div>
       </div>
-      <div class="overview-right">
-        <div class="score-wrapper">
-          <span class="title">服务态度</span>
-          <star :size="36" :score="seller.serviceScore"></star>
-          <span class="score">{{seller.serviceScore}}</span>
-        </div>
-        <div class="score-wrapper">
-          <span class="title">商品评分</span>
-          <star :size="36" :score="seller.foodScore"></star>
-          <span class="score">{{seller.foodScore}}</span>
-        </div>
-        <div class="delivery-wrapper">
-          <span class="title">送达时间</span>
-          <span class="delivery">{{seller.deliveryTime}}分钟</span>
-        </div>
+      <split></split>
+      <ratingselect :select-type="selectType" :only-content="onlyContent" :desc="desc"
+                    :ratings="ratings"></ratingselect>
+      <div class="rating-wrapper">
+        <ul>
+          <li v-for="(rating,index) in ratings" v-bind:key="index" class="rating-item">
+            <div class="avatar">
+              <img :src="rating.avatar" width="28" height="28">
+            </div>
+            <div class="content">
+              <h1 class="name">{{rating.username}}</h1>
+              <div class="star-wrapper">
+                <star :size="24" :score="rating.score"></star>
+                <span class="delivery" v-show="rating.deliveryTime">{{rating.deliveryTime}}分钟送达</span>
+              </div>
+              <p class="text">{{rating.text}}</p>
+              <div class="recommend">
+                <span class="icon-thumb_up"></span>
+                <span v-for="(item,index) in rating.recommend" v-bind:key="index">{{item}}</span>
+              </div>
+              <div class="time">
+                {{rating.rateTime | formatDate}}
+              </div>
+            </div>
+          </li>
+        </ul>
       </div>
     </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+  import BScroll from 'better-scroll';
   import star from '../../components/star/star';
+  import split from 'components/split/split';
+  import ratingselect from 'components/ratingselect/ratingselect';
+  import {formatDate} from 'common/js/date';
 
+  const ALL = 2;
+  const ERR_OK = 0;
   export default {
     props: {
       seller: {
         type: Object
       }
     },
+    data() {
+      return {
+        ratings: [],
+        selectType: ALL,
+        onlyContent: true,
+        desc: {
+          all: '全部',
+          positive: '推荐',
+          negative: '吐槽'
+        }
+      };
+    },
     components: {
-      star
+      star,
+      split,
+      ratingselect
+    },
+    created() {
+      this.$http.get('/api/ratings').then((response) => {
+        response = response.body;
+        if (response.errno === ERR_OK) {
+          this.ratings = response.data;
+          this.$nextTick(() => {
+            this.scroll = new BScroll(this.$refs.ratings, {
+              click: true
+            });
+          });
+        }
+      });
+    },
+    filters: {
+      formatDate(time) {
+        let date = new Date(time);
+        return formatDate(date, 'yyyy-MM-dd hh:mm');
+      }
     }
   };
 </script>
