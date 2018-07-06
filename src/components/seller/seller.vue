db4b16020f093155274b1f1d290a44ba6813a9a5
wengbiancheng <1569702426@qq.com>
2018-07-06 23:17:08 +0800

    seller模块完成中间公告部分和优惠信息部分的布局和样式

--- src/components/seller/seller.vue
+++ src/components/seller/seller.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="seller">
+  <div class="seller" ref="seller">
     <div class="seller-content">
       <div class="overview">
         <h1 class="title">{{seller.name}}</h1>
@@ -29,12 +29,27 @@
           </li>
         </ul>
       </div>
+      <split></split>
+      <div class="bulletin">
+        <h1 class="title">公告与活动</h1>
+        <div class="content-wrapper border-1px">
+          <p class="content">{{seller.bulletin}}</p>
+        </div>
+        <ul v-if="seller.supports" class="supports">
+          <li class="support-item border-1px" v-for="(item,index) in seller.supports" v-bind:key="index">
+            <span class="icon" :class="classMap[seller.supports[index].type]"></span>
+            <span class="text">{{seller.supports[index].description}}</span>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
   import star from '../../components/star/star';
+  import split from '../../components/split/split';
+  import BScroll from 'better-scroll';
 
   export default {
     props: {
@@ -43,7 +58,34 @@
       }
     },
     components: {
-      star
+      star,
+      split
+    },
+    created() {
+      this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
+    },
+    mounted() {
+      this.$nextTick(() => {
+        this._initScroll();
+      });
+    },
+    watch: {
+      seller: function (val) {
+        this.$nextTick(() => {
+          this._initScroll();
+        });
+      }
+    },
+    methods: {
+      _initScroll: function () {
+        if (!this.scroll) {
+          this.scroll = new BScroll(this.$refs.seller, {
+            click: true
+          });
+        } else {
+          this.scroll.refresh();
+        }
+      }
     }
   };
 </script>
@@ -52,7 +94,7 @@
   @import "../../common/stylus/mixin.styl"
   .seller
     position: absolute
-    top: 174px
+    top: 174 px
     bottom: 0px
     left: 0px
     width: 100%
@@ -100,4 +142,45 @@
             color: rgb(7, 17, 27)
             .stress
               font-size: 24px
+    .bulletin
+      padding: 18px 18px 0px 18px
+      .title
+        margin-bottom: 8px
+        line-height: 14px
+        font-size: 14px
+        color: rgb(7, 17, 27)
+      .content-wrapper
+        padding: 0px 12px 16px 12px
+        border-1px(rgba(7, 17, 27, 0.1))
+        .content
+          line-height: 24px
+          font-size: 12px
+          color: rgb(240, 20, 20)
+    .supports
+      .support-item
+        padding: 16px 12px
+        border-1px(rgba(7, 17, 27, 0.1))
+        font-size: 0px
+        .icon
+          display: inline-block
+          vertical-align: top
+          width: 16px
+          height: 16px
+          margin-right: 6px
+          background-size: 16px 16px
+          background-repeat: no-repeat
+          &.decrease
+            bg-image('decrease_4')
+          &.discount
+            bg-image('discount_4')
+          &.guarantee
+            bg-image('guarantee_4')
+          &.invoice
+            bg-image('invoice_4')
+          &.special
+            bg-image('special_4')
+        .text
+          line-height: 16px
+          font-size: 12px
+          color: rgb(7, 17, 27)
 </style>
