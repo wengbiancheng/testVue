8115b29da9f85182b84b44e1766bbbafe2cd37ab
wengbiancheng <1569702426@qq.com>
2018-07-14 23:27:21 +0800

    seller模块完成收藏商家控件的localStorage缓存逻辑

--- src/components/seller/seller.vue
+++ src/components/seller/seller.vue
@@ -72,6 +72,7 @@
   import star from '../../components/star/star';
   import split from '../../components/split/split';
   import BScroll from 'better-scroll';
+  import {saveToLocal, loadFromLocal} from "../../common/js/store";
 
   export default {
     props: {
@@ -81,7 +82,9 @@
     },
     data() {
       return {
-        favorite: false
+        favorite: (() => {
+          return loadFromLocal(this.seller.id, 'favorite', false);
+        })()
       };
     },
     computed: {
@@ -143,6 +146,7 @@
           return;
         }
         this.favorite = !this.favorite;
+        saveToLocal(this.seller.id, 'favorite', this.favorite);
       }
     }
   };
